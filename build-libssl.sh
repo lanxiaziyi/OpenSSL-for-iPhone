f1d788ec68f427a2c2fabb77008e9cc077ddf949
lanxia <ziyi.001@163.com>
2015-08-24 20:07:23 +0800

    Revert "Merge remote-tracking branch 'x2on/master'"
    This reverts commit 50a303122a77296391696bc90d256fd3f39c7127, reversing
    changes made to 149c9c00ec998b697d4cff9d1a2529c5da47238c.

--- build-libssl.sh
+++ build-libssl.sh
@@ -23,10 +23,6 @@
 #				
 VERSION="1.0.2d"													      #
 SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`														  #
-CONFIG_OPTIONS=""
-
-# To set "enable-ec_nistp_64_gcc_128" configuration for x64 archs set next variable to "true"
-ENABLE_EC_NISTP_64_GCC_128=""
 #																		  #
 ###########################################################################
 #																		  #
@@ -96,15 +92,6 @@ do
 	echo "Building openssl-${VERSION} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
 	echo "Please stand by..."
 
-	LOCAL_CONFIG_OPTIONS="${CONFIG_OPTIONS}"
-	if [ "${ENABLE_EC_NISTP_64_GCC_128}" == "true" ]; then
-		case "$ARCH" in
-			*64*)
-				LOCAL_CONFIG_OPTIONS="${LOCAL_CONFIG_OPTIONS} enable-ec_nistp_64_gcc_128"
-			;;
-		esac
-	fi
-
 	if [ "${SDKVERSION}" == "9.0" ]; then
 		export CC="${BUILD_TOOLS}/usr/bin/gcc -arch ${ARCH} -fembed-bitcode"
 	else
@@ -116,10 +103,10 @@ do
 
 	set +e
 	if [ "${ARCH}" == "x86_64" ]; then
-	    ./Configure no-asm darwin64-x86_64-cc --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" ${LOCAL_CONFIG_OPTIONS} > "${LOG}" 2>&1
-    	else
-	    ./Configure iphoneos-cross --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" ${LOCAL_CONFIG_OPTIONS} > "${LOG}" 2>&1
-	fi
+	    ./Configure no-asm darwin64-x86_64-cc --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" > "${LOG}" 2>&1
+    else
+	    ./Configure iphoneos-cross --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" > "${LOG}" 2>&1
+    fi
     
     if [ $? != 0 ];
     then 
@@ -132,14 +119,8 @@ do
 
 	if [ "$1" == "verbose" ];
 	then
-		if [[ ! -z $CONFIG_OPTIONS ]]; then
-			make depend
-		fi
 		make
 	else
-		if [[ ! -z $CONFIG_OPTIONS ]]; then
-			make depend >> "${LOG}" 2>&1
-		fi
 		make >> "${LOG}" 2>&1
 	fi
 	
@@ -150,7 +131,7 @@ do
     fi
     
     set -e
-	make install_sw >> "${LOG}" 2>&1
+	make install >> "${LOG}" 2>&1
 	make clean >> "${LOG}" 2>&1
 done
 
